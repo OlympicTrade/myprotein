b05701eb93a5f9375546fa27a86d61b094c26f9c
VKS PC <vks.ecommerce@gmail.com>
2019-10-24 15:02:26 +0300

    push

--- public_html/js/libs/elements.js
+++ public_html/js/libs/elements.js
@@ -69,82 +69,6 @@ let InputCounter = function(el, options) {
     this.setCount(this.counter)
 };
 
-/*
-class InputCounter {
-    obj = this;
-
-    constructor(el, options) {
-        this.el = el;
-        this.input = $('input', el);
-        this.incr  = $('.incr', el);
-        this.decr  = $('.decr', el);
-
-        this.counter = parseInt(this.input.val());
-        this.min = this.input.attr('min') ? parseInt(this.input.attr('min')) : 1;
-        this.max = this.input.attr('max') ? parseInt(this.input.attr('max')) : 999;
-
-        let obj = this;
-
-        this.incr.on('click', function() {
-            obj.setCount(obj.counter + 1);
-        });
-
-        this.decr.on('click', function() {
-            obj.setCount(obj.counter - 1);
-        });
-
-        this.setCount(this.counter)
-    }
-
-    setMax(max) {
-        this.max = parseInt(max);
-        this.counter > max ? this.setCount(max) : this.setCount(this.counter);
-        return this;
-    };
-
-    setMin(min) {
-        this.min = parseInt(min);
-        this.counter < min ? this.setCount(min) : this.setCount(this.counter);
-        return this;
-    };
-
-    getCount() {
-        return this.counter;
-    }
-
-    changeTimer = null;
-    setCount(newCount) {
-        newCount = parseInt(newCount);
-        if(newCount >= this.max) {
-            newCount = this.max;
-            this.incr.addClass('disabled');
-        } else {
-            this.incr.removeClass('disabled');
-        }
-
-        if(newCount <= this.min) {
-            newCount = this.min;
-            this.decr.addClass('disabled');
-        } else {
-            this.decr.removeClass('disabled');
-        }
-
-        if(newCount === this.counter)
-            return this;
-
-        this.counter = newCount;
-        this.input.val(this.counter);
-
-        let obj = this;
-        clearTimeout(this.changeTimer);
-        this.changeTimer = setTimeout(function() {
-            obj.input.trigger('change');
-        }, 250);
-
-        return this;
-    };
-}
-*/
 function pluginGenerator(els, className, options) {
     els = $(els);
 
@@ -171,6 +95,29 @@ function pluginGenerator(els, className, options) {
     return this;
 }
 
+$.fn.inputCounter = function (options) {
+    let initDE = function(el) {
+        let sl = el.data('db-input-counter');
+
+        if (sl === undefined || sl === '') {
+            sl = new InputCounter(el, options);
+            el.data('db-input-counter', sl);
+        }
+
+        return sl;
+    };
+
+    if($(this).length === 1) {
+        return initDE($(this));
+    }
+
+    $(this).each(function () {
+        initDE($(this));
+    });
+
+    return this;
+};
+/*
 $.fn.inputCounter = function () {
     return pluginGenerator(this, 'InputCounter');
-};
+};*/
