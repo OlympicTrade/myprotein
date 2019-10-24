c7d8d00eef649e703310d765cd162dbc5794ffee
VKS PC <vks.ecommerce@gmail.com>
2019-10-24 14:44:56 +0300

    push

--- public_html/js/libs/elements.js
+++ public_html/js/libs/elements.js
@@ -1,3 +1,78 @@
+let InputCounter = function() {
+    obj = this;
+
+    this.setMax = function(max) {
+        this.max = parseInt(max);
+        this.counter > max ? this.setCount(max) : this.setCount(this.counter);
+        return this;
+    };
+
+    this.setMin = function(min) {
+        this.min = parseInt(min);
+        this.counter < min ? this.setCount(min) : this.setCount(this.counter);
+        return this;
+    };
+
+    this.getCount = function() {
+        return this.counter;
+    };
+
+    this.changeTimer = null;
+    this.setCount = function(newCount) {
+        newCount = parseInt(newCount);
+        if(newCount >= this.max) {
+            newCount = this.max;
+            this.incr.addClass('disabled');
+        } else {
+            this.incr.removeClass('disabled');
+        }
+
+        if(newCount <= this.min) {
+            newCount = this.min;
+            this.decr.addClass('disabled');
+        } else {
+            this.decr.removeClass('disabled');
+        }
+
+        if(newCount === this.counter)
+            return this;
+
+        this.counter = newCount;
+        this.input.val(this.counter);
+
+        let obj = this;
+        clearTimeout(this.changeTimer);
+        this.changeTimer = setTimeout(function() {
+            obj.input.trigger('change');
+        }, 250);
+
+        return this;
+    };
+
+    this.el = el;
+    this.input = $('input', el);
+    this.incr  = $('.incr', el);
+    this.decr  = $('.decr', el);
+
+    this.counter = parseInt(this.input.val());
+    this.min = this.input.attr('min') ? parseInt(this.input.attr('min')) : 1;
+    this.max = this.input.attr('max') ? parseInt(this.input.attr('max')) : 999;
+
+    let obj = this;
+
+    this.incr.on('click', function() {
+        obj.setCount(obj.counter + 1);
+    });
+
+    this.decr.on('click', function() {
+        obj.setCount(obj.counter - 1);
+    });
+
+    this.setCount(this.counter)
+};
+
+
+/*
 class InputCounter {
     obj = this;
 
@@ -71,8 +146,7 @@ class InputCounter {
 
         return this;
     };
-}
-
+}*/
 function pluginGenerator(els, className, options) {
     els = $(els);
 
